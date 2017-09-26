c8943bd7f6b70484529eae2a8bc6bf7a8fab35d8
Travis Morton <tmorton@bandwidth.com>
2017-09-26 14:45:25 -0400

    handled some cases where contact_ner wasn't returning correct contact

--- rasa_nlu/extractors/contact_entity_extractor.py
+++ rasa_nlu/extractors/contact_entity_extractor.py
@@ -30,25 +30,42 @@ class ContactEntityExtractor(EntityExtractor):
 
     def extract_entities(self, text, contacts, s_doc):
         # type: (Doc) -> List[Dict[Text, Any]]
-        _contacts = [
+        _contacts = []
+        for span in s_doc.noun_chunks:
+            _contacts.append(
+            {
+                "entity": "contact",
+                "value": span.text,
+                "start": span.start_char,
+                "end": span.end_char,
+                "match": "spacy_noun"
+            })
+
+        _exact_contacts = [
             {
                 "entity": "contact",
                 "value": contact,
                 "start": str(text.find(contact)),
-                "end": str(text.find(contact) + len(contact)-1),
+                "end": str(text.find(contact)+len(contact)),
                 "match": "exact"
             }
             for contact in contacts if contact in text]
-        _contacts = [max(_contacts, key=lambda c: len(c['value']))]
 
-        if len(_contacts) == 0:
-            _contacts = [
-                {
-                    "entity": "contact",
-                    "value": noun.text,
-                    "start": noun.start_char,
-                    "end": noun.end_char,
-                    "match": "spacy_noun"
-                }
-                for noun in s_doc.noun_chunks]
+        _contacts = self.merge_contacts(_contacts, _exact_contacts, 'value')
+
+        # take the maximum length contact assuming that will be the most precise
+        # match "Drew" vs. "Drew G's cell"
+        try:
+            _contacts = [max(_contacts, key=lambda c: len(c['value']))]
+        except ValueError:
+            _contacts = []
         return _contacts
+
+    def merge_contacts(self, list1, list2, key):
+        merged = {}
+        for item in list1+list2:
+            if item[key] in merged:
+                merged[item[key]].update(item)
+            else:
+                merged[item[key]] = item
+        return merged.values()
